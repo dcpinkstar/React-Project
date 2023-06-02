8237b3361ddd628e285a31ef76f40024764b145e
Casey Hatcher <casey.willetts@gmail.com>
2023-06-02 15:54:26 -0400

    CampsitesDirectoryPage and CampsitesDetail

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import './App.css';
-import CampsitesList from './features/campsites/CampsitesList';
 
 function App() {
     return (
         <div className='App'>
             <Header />
-            <CampsitesList />
+            <CampsitesDirectoryPage />
             <Footer />
         </div>
     );
-};
+}
 
 export default App;
